ca5c220f6577fce972b7c4dd4b2eb62a8bfd65b2
mslim8803 <mslim8803@kaoni.com>
2020-08-28 17:34:03 +0900

    first commit

---
+++ src/java/JustForTest.java
@@ -0,0 +1,7 @@
+package java;
+
+public class JustForTest {
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
---
+++ src/java/ch02/Type.java
@@ -0,0 +1,5 @@
+package java.ch02;
+
+public enum Type {
+	DOG, CAT, MONKEY
+}
